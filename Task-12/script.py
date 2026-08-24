ab58d32c4f3f4954f4e00e2ac247d3b0fa6f430a
A17PRO <adityamathew75@gmail.com>
2026-08-24 10:55:23 +0530

    Make functions to call them easier and apply to wallpaper

--- Task-12/script.py
+++ Task-12/script.py
@@ -1,22 +1,57 @@
+import os
+import subprocess
+import textwrap
+
 from PIL import Image, ImageFont, ImageDraw, ImageColor, ImageFilter
 from google import genai
 from dotenv import load_dotenv
 from datetime import datetime
-'''
-with open("to-do-list.txt", "r") as file:
-    content = file.read()
-    print(content)
-'''
-time = datetime.now().strftime("%H:%M:%S")
-print(time)
 
+#FUNCTIONS
+def draw_time(draw, time, font, color):
+    draw.text((1600, 100), time, font=font, fill=color)
+
+
+def read_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r") as file:
+            return file.read()
+    else:
+        return "File not found"
+
+def draw_notes(draw, text, font, color):
+    lines = text.split('\n')
+
+    wrapped_lines = []
+    for line in lines:
+        wrapped_chunks = textwrap.wrap(line, width = 45)
+        wrapped_lines.extend(wrapped_chunks)
+
+    formatted_text = "\n".join(wrapped_lines)
+    draw.multiline_text((100, 100), formatted_text, font=font, fill=color, spacing = 15)
+
+
+
+#EXECUTION - MAIN CODE
+#init
 canvas = Image.new("RGB", (1920, 1080), ImageColor.getrgb("#000000"))
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 40)
+time = datetime.now().strftime("%H:%M:%S")
 
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 20)
-
+#draw time
 draw = ImageDraw.Draw(canvas)
-draw.text((1600, 100), time, font=ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30), fill=(255, 255, 255))
+draw_time(draw, time, font, (255,255,255))
 
+#draw notes
+notes = read_file("to-do-list.txt")
+draw_notes(draw, notes, font, (255, 255, 255))
+
+#save wallpaper
 wallpaper_path = "current_wallpaper.jpg"
 canvas.save(wallpaper_path)
 print(f"Saved successfully to {wallpaper_path}")
+
+#apply wallpaper
+abs_path = os.path.abspath(wallpaper_path)
+subprocess.run(["plasma-apply-wallpaperimage", abs_path])
+print("Wallpaper Applied")
